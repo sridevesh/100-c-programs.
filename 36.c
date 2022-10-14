d73dfa3227f3665b5e3fcc64efc79e1ec6e3dc57
sridevesh <113873686+sridevesh@users.noreply.github.com>
2022-10-14 14:35:33 +0530

    nested if else

---
+++ 36.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+    printf("Register number: RA2211042010056\n");
+    int number1, number2;
+    printf("Enter two integers: ");
+    scanf("%d %d", &number1, &number2);
