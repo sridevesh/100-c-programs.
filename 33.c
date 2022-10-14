6736f819dbbcdef64fb33a2699470b147d83ceb0
sridevesh <113873686+sridevesh@users.noreply.github.com>
2022-10-14 14:23:23 +0530

    to find a number positive and negative

---
+++ 33.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+int main() 
+{
+  printf("Register number: RA2211042010007\n");
+  int myNum = 10;
+
+  if (myNum > 0)
+  {
+    printf("The value is a positive number.");
+  }
+  else if (myNum < 0)
+  {
+    printf("The value is a negative number.");
+  }
+  else
+  {
+    printf("The value is 0.");
+
+  return 0;
+  } 
+}
