86435c89bc6a811703fe4fdea4abd14c7d817019
sridevesh <113873686+sridevesh@users.noreply.github.com>
2022-10-14 14:25:21 +0530

    find average and deviation

---
+++ 34.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+int main()
+{
+    printf("Register number: RA2211042010007\n");
+    int num1;
+    int num2;
+    int num3;
+@@ -30,5 +32,5 @@ int main() {
+    printf("\nsecond number: %6d -- deviation: %8.2f",num2,num2-average);
+    printf("\nthird number: %6d -- deviation: %8.2f",num3,num3-average);
+    printf("\nfourth number: %6d -- deviation: %8.2f",num4,num4-average);
+    return 0;
+
+}
