9a3763eaff3d6198202aecd373ba7911c7821cf1
sridevesh <113873686+sridevesh@users.noreply.github.com>
2022-10-14 14:27:20 +0530

    convert radians to degrees

---
+++ 35.c
@@ -0,0 +1,7 @@
+#define degree_factor 57.295779
+int main()
+{
+    printf("Register number: RA2211042010056\n");
+    double radians;
+    double degrees;
+    printf("enter the angle in radians: ");
