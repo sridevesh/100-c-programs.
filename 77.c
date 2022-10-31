9e7e59eed60c318fa7cad34db24d7675be4b3fdc
sridevesh <113873686+sridevesh@users.noreply.github.com>
2022-10-31 23:09:21 +0530

    == operator

---
+++ 77.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  printf("Register number: RA2211042010056\n");
+  int x = 5;
+  int y = 3;
+  printf("%d", x == y); // returns 0 (false) because 5 is not equal to 3
+  return 0;
+}
