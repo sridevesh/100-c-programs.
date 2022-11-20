0565dd813388e43b49b25cc8a1d851621942ee6f
sridevesh <113873686+sridevesh@users.noreply.github.com>
2022-11-20 22:36:01 +0530

    Create 91.c

---
+++ 91.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Register number: RA2211042010056\n\n");
+    float a,b,sum;
+    scanf("%f%f",&a,&b);
+    sum = a+b;
+    printf("%0.3f",sum);
+
+    return 0;
+}
