e6ee38b82ebe50be2cfe864f9cf2560c9a4f080a
sridevesh <113873686+sridevesh@users.noreply.github.com>
2022-11-20 22:40:19 +0530

    codetantra 8

---
+++ 93.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Register number: RA2211042010056\n\n");
+    float a,b,c;
+    scanf("%f %f",&a,&b);
+    c = a*b;
+    printf("%0.4f",c);
+
+    return 0;
+}
