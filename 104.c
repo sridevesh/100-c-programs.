718463bf8defe6c36221d3bab834e85a52833ae9
sridevesh <113873686+sridevesh@users.noreply.github.com>
2022-11-22 10:50:52 +0530

    print pattern asterisks

---
+++ 104.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    printf("Register number: RA2211042010056\n\n");
+    printf("*\n");
+    printf("**\n");
+    printf("***\n");
+    printf("****\n");
+    
+    return 0;
+}
